2e77981914de3678f78d7c0f08fa2641a21b4625
Bruno Bretas Maurrique <89142175+LOLOL538@users.noreply.github.com>
2022-09-22 15:03:34 -0300

    Add files via upload

---
+++ Vetor/src/Ex6.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		
+		    
+		         Scanner teclado = new Scanner(System.in);
+		         
+					int nota1, nota2, nota3;
+					int media, i, Total = 0;
+					
+					for(i = 0; i < 3; i++){
+					    
+					    Total++;
+					    
+					    System.out.println("Aluno " + Total + ", digite sua 1ª nota");
+					    nota1 = teclado.nextInt();
+					    
+					    
+					    System.out.println("Aluno " + Total + ", digite sua 2ª nota");
+					    nota2 = teclado.nextInt();
+					    
+					    
+					    System.out.println("Aluno " + Total + ", digite sua 3ª nota");
+					    nota3 = teclado.nextInt();
+					    
+					    
+					    media = (nota1 + nota2 + nota3) / 3;
+					    System.out.println("A média do aluno " + Total + " é " + media);
+					    
+					    
+					    if( (media >= 0) && (media <4) ){
+					        System.out.println("Nota E");
+					    } else if(media < 5){
+					        System.out.println("Nota D");
+					    } else if(media < 7){
+					        System.out.println("Nota C");
+					    } else if(media < 8){
+					        System.out.println("Nota B");
+					    } else if(media <= 10){
+					        System.out.println("Nota A");
+					    }
+					}
+				} 
+	}
+
---
+++ Vetor/src/Exerc6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exerc6 {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+
+	}
+
+}
